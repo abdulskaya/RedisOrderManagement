17a78274169d97abf03b604daad36e9dabce4bd8
abdulskaya <abdulsamet.kaya@pixasoftware.com>
2024-10-05 16:30:17 +0300

    memory cache integrated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { Order } from './order/entities/order.entity';
+import { OrderModule } from './order/order.module';
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 1000 * 30000
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/order/order.service.ts
@@ -0,0 +1,57 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+
+@Injectable()
+export class OrderService {
+  constructor(@Inject('CACHE_MANAGER') private cacheManager: Cache) { }
+
+
+  async setOrders() {
+    await this.cacheManager.set('key1', 'hello');
+  }
+
+  async getOrders() {
+    let cacheData: any = await this.cacheManager.get('students');
+    if (cacheData) {
+      return cacheData;
+    }
+    let studentsData = await this.retrieveStudentsFromDb();
+    this.cacheManager.set('students', studentsData);
+    return studentsData;
+  }
+
+  async retrieveStudentsFromDb() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const students = [
+          { name: "John", age: 20, GPA: 3 },
+          { name: "Kemal", age: 23, GPA: 5 },
+          { name: "Takachi", age: 24, GPA: 2 }
+        ];
+        resolve(students);
+      }, 1000)
+    })
+  }
+
+  create(createOrderDto: CreateOrderDto) {
+    return 'This action adds a new order';
+  }
+
+  findAll() {
+    return `This action returns all order`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} order`;
+  }
+
+  update(id: number, updateOrderDto: UpdateOrderDto) {
+    return `This action updates a #${id} order`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} order`;
+  }
+}
